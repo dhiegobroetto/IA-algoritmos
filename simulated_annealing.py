b6e23b8715500a5f7ad6af48affd97efd6e9d095
dhiegobroetto <dhiegobroetto@hotmail.com>
2019-09-25 12:52:39 -0300

    Alguns ajustes necessitam ser feitos

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -2,6 +2,8 @@
 # ---------- 2016204404 ----------- #
 
 from random import shuffle
+import math
+import random
 
 def getValueState(VT, states) :
     total_value = 0
@@ -35,29 +37,43 @@ def defineNeighborhood(VT, state, states_list) :
     getPositiveNeighbor(state, states_list)
     getNegativeNeighbor(state, states_list)   
 
-def simulated_annealing(VT, max_size, t, alpha, state) :
-    # best_value = 0
-    # best_state = [0] * len(VT)
-    # while(True) :
-    #     find_best = False
-    #     defineNeighborhood(VT, best_state, states_list)
-    #     shuffle(states_list)
-    #     while(states_list != []) :
-    #         state = states_list.pop()
-    #         if(max_size >= getSizeState(VT, state)):
-    #             if(best_value < getValueState(VT, state)) :
-    #                 best_value = getValueState(VT, state)
-    #                 best_state = state
-    #                 find_best = True
-    #                 states_list.clear()
-    #     if(not find_best) :
-    #         break
+def probabilityState(worst_case,state,t) :
+    p = 1/math.exp(1)**((worst_case-state)/t)
+    r = random.uniform(0,1)
+    return r < p
+
+def simulated_annealing(VT, max_size, t, alpha, states_list) :
+    best_value = 0
+    best_state = [0] * len(VT)
+    while(t >= 1) :
+        find_best = False
+        defineNeighborhood(VT, best_state, states_list)
+        shuffle(states_list)
+        while(states_list != []) :
+            state = states_list.pop()
+            if(max_size >= getSizeState(VT, state)):
+                if(best_value < getValueState(VT, state)) :
+                    best_value = getValueState(VT, state)
+                    best_state = state
+                    find_best = True
+                    states_list.clear()
+                else :
+                    if(probabilityState(getValueState(VT, state),getValueState(VT,best_state),t)) :
+                        best_value = getValueState(VT, state)
+                        best_state = state
+                        find_best = True
+                        break
+        if(not find_best) :
+            break
+        t *= alpha
     return state
 
 # Max size
 max_size = 19 
 # Object array
 VT = [(1, 3), (4, 6), (5, 7)]
+t = 100
+alpha = 0.5
 
 # Trivial solution
 # states = [0] * len(VT)
@@ -65,17 +81,14 @@ VT = [(1, 3), (4, 6), (5, 7)]
 
 # Simulated Annealing
 states_list = []
-best_state_simple = simulated_annealing(VT, max_size, best_state_trivial, states_list)
+best_simulated_annealing = simulated_annealing(VT, max_size, t, alpha, states_list)
 
 # Results
-total_value_trivial = getValueState(VT, best_state_trivial)
-total_size_trivial = getSizeState(VT, best_state_trivial)
+# total_value_trivial = getValueState(VT, best_state_trivial)
+# total_size_trivial = getSizeState(VT, best_state_trivial)
 
-total_value_simple = getValueState(VT, best_state_simple)
-total_size_simple = getSizeState(VT, best_state_simple)
-
-print("Trivial Solution")
-print ("[Total Value => ", total_value_trivial, ", Total Size => ", total_size_trivial, ", Best State => ", best_state_trivial)
+total_value_simple = getValueState(VT, best_simulated_annealing)
+total_size_simple = getSizeState(VT, best_simulated_annealing)
 
 print("Simple Descent")
-print ("[Total Value => ", total_value_simple, ", Total Size => ", total_size_simple, ", Best State => ", best_state_simple)
+print ("[Total Value => ", total_value_simple, ", Total Size => ", total_size_simple, ", Best State => ", best_simulated_annealing)
